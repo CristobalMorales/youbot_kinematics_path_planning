39471bc404f6da05809e216d03c4c9429492a129
Cristobal Morales <cristobal.morales92@gmail.com>
2022-04-17 23:11:32 -0400

    #comment Add code of youbot forward and inverse kinematics, path plannig with dijkstra and trial for 6 points

---
+++ path_planning/src/path_planning_node.cpp
@@ -0,0 +1,455 @@
+#include "ros/ros.h"
+#include "youbot_fk_ik/youbotKine.h"
+#include "rosbag/bag.h"
+#include "rosbag/view.h"
+#include "trajectory_msgs/JointTrajectory.h"
+#include "geometry_msgs/Point.h"
+
+#include "boost/foreach.hpp"
+#include <string>
+
+#include <unsupported/Eigen/MatrixFunctions>
+#include <iostream>
+#include <cmath>
+#include "visualization_msgs/MarkerArray.h"
+
+#include <list>
+
+using namespace Eigen;
+#define POINTS_DATA 500
+trajectory_msgs::JointTrajectoryPoint traj_pt;
+
+//TODO: You can design your code to achieve the q6 task 
+//however you want as long as it stays in this file and it runs.
+
+struct points {
+    Matrix4d transform;
+};
+
+void show_degrees(double points[], char name[]) 
+{
+    std::cout << "Angulos de " << name << ": \n" << std::endl;
+    for (int i = 0; i<5;i++)
+        std::cout << points[i]*180/M_PI << std::endl;
+}
+
+MatrixXd quaternion_rotation(double q[]) 
+{
+    MatrixXd rotation(3,3);
+    double qx = q[0];
+    double qy = q[1];
+    double qz = q[2];
+    double qw = q[3];
+    rotation(0,0) = 1 - 2*qy*qy - 2*qz*qz; 
+    rotation(0,1) = 2*qx*qy - 2*qz*qw; 
+    rotation(0,2) = 2*qx*qz + 2*qy*qw;
+    rotation(1,0) = 2*qx*qy + 2*qz*qw; 
+    rotation(1,1) = 1 - 2*qx*qx - 2*qz*qz;
+    rotation(1,2) = 2*qy*qz - 2*qx*qw;
+    rotation(2,0) = 2*qx*qz - 2*qy*qw;
+    rotation(2,1) = 2*qy*qz + 2*qx*qw; 
+    rotation(2,2) = 1 - 2*qx*qx - 2*qy*qy;
+    return rotation;
+}
+
+bool intesect_object(double *data) {
+    double object_1[3][2] = { 
+        0.25751, 0.37751, 
+        0, 0.13, 
+        0, 0.14 };// cilindro 1
+    double object_2[3][2] = { 
+        -0.378049, -0.278049, 
+        -0.276524, -0.176524, 
+        0, 0.08 };// cilindro 0
+    double object_3[3][2] = { 
+        -0.4118985, -0.2178655,
+        -0.200138, -0.1231,
+        0, 0.2};// caja 
+    bool intersect = false;
+    if (data[0] > object_1[0][0] && data[0] < object_1[0][1]){
+        if (data[1] > object_1[1][0] && data[1] < object_1[1][1]){
+            if (data[2] > object_1[2][0] && data[2] < object_1[2][1]){
+                intersect = true;
+            }
+        }
+    }
+    if (data[0] > object_2[0][0] && data[0] < object_2[0][1]){
+        if (data[1] > object_2[1][0] && data[1] < object_2[1][1]){
+            if (data[2] > object_2[2][0] && data[2] < object_2[2][1]){
+                intersect = true;
+            }
+        }
+    }
+    if (data[0] > object_3[0][0] && data[0] < object_3[0][1]){
+        if (data[1] > object_3[1][0] && data[1] < object_3[1][1]){
+            if (data[2] > object_3[2][0] && data[2] < object_3[2][1]){
+                intersect = true;
+            }
+        }
+    }
+    return intersect;
+}
+
+void generate_points(double intervals[3][2], double data[POINTS_DATA][3])
+{
+    double x_interval[2] = {intervals[0][0], intervals[0][1]};
+    double y_interval[2] = {intervals[1][0], intervals[1][1]};
+    double z_interval[2] = {intervals[2][0], intervals[2][1]};
+    int n_data = 0;
+    while (n_data < POINTS_DATA)
+    {
+        double data_aux[3];
+        for (int i = 0; i < 3; i++) 
+        {
+            double range = intervals[i][1] - intervals[i][0];
+            double rand_num = ((double) rand() / (RAND_MAX));
+            data_aux[i] = rand_num*range + intervals[i][0];
+        }
+        if (!intesect_object(data_aux)){
+            data[n_data][0] = data_aux[0];
+            data[n_data][1] = data_aux[1];
+            data[n_data][2] = data_aux[2];
+            n_data++;
+        }
+    }
+}
+
+std::list<double*> connect_points(double data[POINTS_DATA+5][3]){
+    std::list<double*> connections;
+    double distance_limit = 0.1;
+    double interpolate_points = 50;
+    for (int i = 0; i < POINTS_DATA + 5; i++)
+    {
+        double x_pos = data[i][0];
+        double y_pos = data[i][1];
+        double z_pos = data[i][2];
+        for (int j = i + 1; j < POINTS_DATA + 5;j++) 
+        {
+            double x_comp = data[j][0];
+            double y_comp = data[j][1];
+            double z_comp = data[j][2];
+            double distance = sqrt(pow(x_comp - x_pos, 2) + pow(y_comp - y_pos, 2) + pow(z_comp - z_pos, 2));
+            bool is_connection = false;
+            if (distance < distance_limit) 
+            {
+                is_connection = true;
+                double x_pace = (x_comp - x_pos)/interpolate_points;
+                double y_pace = (y_comp - y_pos)/interpolate_points;
+                double z_pace = (z_comp - z_pos)/interpolate_points;
+                for (int k = 1; k <= interpolate_points; k++)
+                {
+                    double data_aux[3];
+                    data_aux[0] = x_pos + x_pace*k;
+                    data_aux[1] = y_pos + y_pace*k;
+                    data_aux[2] = z_pos + z_pace*k;
+                    if (intesect_object(data_aux))
+                    {
+                        is_connection = false;
+                        break;
+                    }
+                }
+            }
+            if (is_connection)
+            {
+                connections.push_back(new double[3] {(double)i, (double)j, distance});
+            }
+        }
+    }
+    return connections;
+}
+
+std::list<double*> dijkstra(std::list<double*> links, int init, int end)
+{
+    std::list<std::list<double*>> paths;
+    std::list<int> used;
+    int current_data_id = init;
+    while (true)
+    {
+        int link_id = 0;
+        std::list<double*> current_path;
+        bool used_data = true;
+        bool all_complete = false;
+        for (std::list<std::list<double*>>::iterator pt = paths.begin(); pt != paths.end(); pt++)
+        { 
+            all_complete = true;
+            used_data = false;
+            for (std::list<int>::iterator us = used.begin(); us != used.end(); us++)
+            {
+                if (*us == (*pt).back()[2])
+                    used_data = true;
+            }
+            if (used_data)
+                continue;
+            double min_dist = 100000000;
+            if (min_dist > (*pt).back()[1])
+            {
+                min_dist = (*pt).back()[1];
+                current_path = (*pt);
+                current_data_id = (*pt).back()[2];
+            }
+            all_complete = false;
+        }
+        if (!current_path.empty() && current_path.back()[2] == (double)end){
+            return current_path;
+        }
+        if (all_complete)
+        {
+            return current_path;
+        }
+        for (std::list<double*>::iterator it = links.begin(); it != links.end(); it++)
+        {
+            double *aux = *it;
+            int next_point = 0;
+            if (aux[0] == current_data_id) {
+                next_point = aux[1];
+            }
+            if (aux[1] == current_data_id) {
+                next_point = aux[0];
+            }
+            bool used_data = false;
+            for (std::list<int>::iterator us = used.begin(); us != used.end(); us++)
+            {
+                if (*us == next_point)
+                    used_data = true;
+            }
+            if (used_data || next_point == 0)
+            {
+                link_id++;
+                continue;
+            }
+            bool is_in_path = false;
+            if (!current_path.empty()){
+            for (std::list<std::list<double*>>::iterator pt = paths.begin(); pt != paths.end(); pt++)
+            {
+                double *current_node = (*pt).back();
+                double *last_node = current_path.back();
+                double distance = last_node[1] + aux[2];
+                if (current_node[2] == next_point && current_node[1] > distance) 
+                {
+                    paths.remove(*pt);
+                    std::list<double*> new_path(current_path);
+                    new_path.push_back(new double [3] {(double)link_id, distance, (double)next_point});//Id del intervalo no it
+                    paths.push_back(new_path);
+                    is_in_path = true;
+                }
+            }
+            }
+            if (current_path.empty())
+            {
+                double distance = aux[2];
+                std::list<double*> new_path;
+                new_path.push_back(new double [3] {(double)link_id, distance, (double)next_point});//Id del intervalo no it
+                paths.push_back(new_path);
+            }
+            else if (!is_in_path)
+            {
+                double *last_node = current_path.back();
+                double distance = last_node[1] + aux[2];
+                std::list<double*> new_path(current_path);
+                new_path.push_back(new double[3] {(double)link_id, distance, (double)next_point});//Id del intervalo no it
+                paths.push_back(new_path);
+            }
+            link_id++;
+        }
+        used.push_back(current_data_id);
+    }
+}
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "youbot_traj_cw2");
+
+    youbot_kinematic youbot;
+    youbot.init();
+    trajectory_msgs::JointTrajectory my_traj;
+    trajectory_msgs::JointTrajectoryPoint my_pt;
+    rosbag::Bag bag;
+    ros::NodeHandle nh;
+    ros::NodeHandle node_handle;
+
+    ros::Publisher traj_pub = nh.advertise<trajectory_msgs::JointTrajectory>("/EffortJointInterface_trajectory_controller/command", 5);
+
+    my_traj.header.stamp = ros::Time::now();
+    my_traj.joint_names.push_back("arm_joint_1");
+    my_traj.joint_names.push_back("arm_joint_2");
+    my_traj.joint_names.push_back("arm_joint_3");
+    my_traj.joint_names.push_back("arm_joint_4");
+    my_traj.joint_names.push_back("arm_joint_5");
+
+    my_pt.positions.resize(5);
+    my_pt.velocities.resize(5);
+    my_pt.accelerations.resize(5);
+
+    int checkpoint_data = 1;//atoi(argv[1]);
+    double tfs = 10;
+    double* steps = new double[5];
+    steps[0] = 1; steps[1] = 1; steps[2] = 50; steps[3] = 1; steps[4] = 50;
+    double* origin_angles = new double[5];
+    origin_angles[0] = 0;origin_angles[1] = 0;origin_angles[2] = 0;
+    origin_angles[3] = 0;origin_angles[4] = 0;
+    MatrixXd last_v(1, 4);
+    last_v(0, 0) = 0; last_v(0, 1) = 0; last_v(0, 2) = 0; last_v(0, 3) = 1;
+    double time_interval = 1;
+    if (checkpoint_data == 1)
+    {
+        bag.open("/home/cristobal/catkin_ws/src/comp0127_lab/cw2/cw2q6/src/../bags/data1.bag", rosbag::bagmode::Read);
+        std::vector<std::string> topics;
+        topics.push_back(std::string("joint_data"));
+        rosbag::View view(bag, rosbag::TopicQuery(topics));
+        double* init_angles = new double[5];
+        double* end_angles = new double[5];
+        int iter_n = 0;
+        double angles[5][5];
+        BOOST_FOREACH(rosbag::MessageInstance const m, view) 
+        {
+            sensor_msgs::JointState::ConstPtr J = m.instantiate<sensor_msgs::JointState>();
+            if (J != NULL)
+            {
+                if (J->position.size() != 0)
+                {
+                    for (int i = 0; i < 5;i++)
+                    {
+                        angles[i][iter_n] = J->position.at(i);
+                    }
+                }
+            }
+            iter_n++;
+        }
+        //int order[5] = {0, 1, 2, 3, 4}; // original
+        int order[5] = {0, 1, 2, 4, 3}; // left to right
+        //int order[5] = {3, 4, 2, 1, 0}; // rigth to left
+        for(int j = 0; j < 5 ; j++) 
+        {
+            double dt = time_interval/steps[j];
+            for (int i = 0; i < 5;i++)
+            {
+                init_angles[i] = end_angles[i];
+                end_angles[i] = angles[i][order[j]];
+            }
+            if (j == 0)
+            {
+                Matrix4d end_pose_matrix = youbot.forward_kine_offset(end_angles, 5);
+                double* new_origin_angles = youbot.inverse_kine_ite(end_pose_matrix, origin_angles);
+                double *joint_outcome = youbot.apply_offset(new_origin_angles);
+                tfs = tfs + ceil(20/steps[j]);
+                my_pt.time_from_start.sec = tfs;
+                for (int pos = 0; pos<5; pos++)
+                {
+                    my_pt.positions.at(pos) = joint_outcome[pos];
+                }
+                my_traj.points.push_back(my_pt);
+                continue;
+            }
+            Matrix4d init_pose_matrix = youbot.forward_kine_offset(init_angles, 5);
+            Matrix4d end_pose_matrix = youbot.forward_kine_offset(end_angles, 5);
+            MatrixXd rot_mat_init = init_pose_matrix.block(0,0,3,3);
+            MatrixXd rot_mat_end = end_pose_matrix.block(0,0,3,3);
+            Vector3d tras_mat_init = init_pose_matrix.block(0,3,3,1);
+            Vector3d tras_mat_end = end_pose_matrix.block(0,3,3,1);
+            double* target_angles = init_angles;
+            for (int k = 1; k <= steps[j]; k++) 
+            {
+                MatrixXd aux_matrix(3,4);
+                MatrixXd t_matrix(4,4);
+                Vector3d aux_pos = tras_mat_init + dt*k*(tras_mat_end - tras_mat_init);
+                MatrixXd aux_rot = rot_mat_init*((rot_mat_init.inverse()*rot_mat_end).log()*dt*k).exp();
+                aux_matrix << aux_rot, aux_pos;
+                t_matrix << aux_matrix, last_v;
+                std::cout << youbot.obtain_pose_vector(t_matrix) << std::endl;
+                double* new_origin_angles = youbot.inverse_kine_ite(t_matrix, origin_angles);
+                double *joint_outcome = youbot.apply_offset(new_origin_angles);
+                tfs = tfs + ceil(20/steps[j]);
+                my_pt.time_from_start.sec = tfs;
+                for (int pos = 0; pos<5; pos++)
+                {
+                    my_pt.positions.at(pos) = joint_outcome[pos];
+                }
+                my_traj.points.push_back(my_pt);
+            }            
+        }
+        sleep(5);
+        traj_pub.publish(my_traj);
+        ros::spinOnce();
+        bag.close();
+        std::cout << "Finished uploading trajectory" << std::endl;
+        return 5;
+    }
+    else if (checkpoint_data == 2)
+    {
+        double intervals[3][2] = {
+            -0.5, 0.5,
+            -0.5, 0.5,
+            0, 0.4};
+        double data_points[POINTS_DATA+5][3];
+
+        bag.open("/home/cristobal/catkin_ws/src/comp0127_lab/cw2/cw2q6/src/../bags/data2.bag", rosbag::bagmode::Read);
+        std::vector<std::string> topics;
+        topics.push_back(std::string("target_position"));
+        rosbag::View view(bag, rosbag::TopicQuery(topics));
+        int iter_n= 0;
+        points *data = new points[5];
+        BOOST_FOREACH(rosbag::MessageInstance const m, view) 
+        {
+            geometry_msgs::Transform::ConstPtr J = m.instantiate<geometry_msgs::Transform>(); 
+            if (J != NULL)
+            {
+                double q[4] = {J->rotation.x, J->rotation.y, J->rotation.z, J->rotation.w};
+                VectorXd translation(3); translation(0) = J->translation.x;
+                translation(1) = J->translation.y; translation(2) = J->translation.z;
+                data_points[POINTS_DATA+iter_n][0] = J->translation.x;
+                data_points[POINTS_DATA+iter_n][1] = J->translation.y;
+                data_points[POINTS_DATA+iter_n][2] = J->translation.z;
+                MatrixXd rotation = quaternion_rotation(q);
+                MatrixXd aux_matrix(3,4);
+                aux_matrix << rotation, translation;
+                data[iter_n].transform << aux_matrix, last_v;
+            }
+            iter_n++;
+        }
+        generate_points(intervals, data_points);
+        std::list<double*> links = connect_points(data_points);
+        Matrix4d end_matrix;
+        dijkstra(links, POINTS_DATA, POINTS_DATA + 1);
+        int order[5] = {0, 1, 4, 3, 2};//0, 1, 2, 4, 3};
+        for(int j = 0; j < 5 ; j++) 
+        {
+            Matrix4d init_matrix = end_matrix;
+            init_matrix = youbot.forward_kine_offset(origin_angles, 5);
+            end_matrix = data[order[j]].transform;
+            if (1)// || j == 2)
+            {
+                //double *new_origin_angles
+                origin_angles = youbot.inverse_kine_ite(end_matrix, origin_angles);
+                origin_angles = youbot.norm_angle(origin_angles);
+                origin_angles = youbot.apply_offset(origin_angles);
+                for (int h = 0; h < 5;h++)
+                    std::cout << origin_angles[h]*180/M_PI << std::endl;
+                std::cout << youbot.forward_kine_offset(origin_angles, 5) << std::endl;
+                tfs = tfs + 15;//ceil(50/steps[j]);
+                my_pt.time_from_start.sec = tfs;
+                for (int pos = 0; pos<5; pos++)
+                {
+                    my_pt.positions.at(pos) = origin_angles[pos];
+                    //my_pt.velocities.at(pos) = 0.02;
+                }
+                my_traj.points.push_back(my_pt);
+            }
+        }
+        sleep(10);
+        traj_pub.publish(my_traj);
+        bag.close();
+        ros::spinOnce();
+        std::cout << "run q6b" << std::endl;
+        std::cout << "Finished uploading trajectory" << std::endl;
+        //Load q6b data
+        return 123;
+    }
+
+    while(ros::ok())
+    {
+        ros::spinOnce();
+        usleep(10);
+    }
+
+    return 123;
+}
---
+++ youbot_fk_ik/src/youbotKine.cpp
@@ -0,0 +1,268 @@
+#include <youbot_fk_ik/youbotKine.h>
+
+void youbot_kinematic::init()
+{
+    /**
+     * @brief Initialize the DH parameters, the offset of the joints and the limits. 
+     * Besides, subscribes to /joint_states topic
+     * 
+     */
+    DH_params[0][0] = -0.033;   DH_params[1][0] = 0.155;  DH_params[2][0] = 0.135; DH_params[3][0] = -0.002;  DH_params[4][0] = 0.0;
+    DH_params[0][1] = M_PI_2;   DH_params[1][1] = 0.0;    DH_params[2][1] = 0.0;   DH_params[3][1] = M_PI_2;  DH_params[4][1] = M_PI;
+    DH_params[0][2] = 0.145;    DH_params[1][2] = 0.0;    DH_params[2][2] = 0.0;   DH_params[3][2] = 0.0;     DH_params[4][2] = -0.185;
+    DH_params[0][3] = M_PI;     DH_params[1][3] = M_PI_2; DH_params[2][3] = 0.0;   DH_params[3][3] = -M_PI_2; DH_params[4][3] = M_PI;
+
+    joint_offset[0] = 170*M_PI/180;
+    joint_offset[1] = 65*M_PI/180;
+    joint_offset[2] = -146*M_PI/180;
+    joint_offset[3] = 102.5*M_PI/180;
+    joint_offset[4] = 167.5*M_PI/180;
+
+    joint_limit_min[0] = -169*M_PI/180;
+    joint_limit_min[1] = -65*M_PI/180;
+    joint_limit_min[2] = -150*M_PI/180;
+    joint_limit_min[3] = -102.5*M_PI/180;
+    joint_limit_min[4] = -167.5*M_PI/180;
+
+    joint_limit_max[0] = 169*M_PI/180;
+    joint_limit_max[1] = 90*M_PI/180;
+    joint_limit_max[2] = 146*M_PI/180;
+    joint_limit_max[3] = 102.5*M_PI/180;
+    joint_limit_max[4] = 167.5*M_PI/180;
+
+    joint_state_sub = nh.subscribe<sensor_msgs::JointState>("/joint_states", 5, &youbot_kinematic::joint_state_callback, this);
+}
+
+void youbot_kinematic::joint_state_callback(const sensor_msgs::JointState::ConstPtr &q)
+{
+    for(int i = 0; i < 5; i++)
+        current_joint_position[i] = q->position.at(i);
+
+    current_pose = forward_kine(current_joint_position, 5);
+    broadcast_pose(current_pose);
+}
+
+Matrix4d youbot_kinematic::dh_matrix_standard(double a, double alpha, double d, double theta)
+{
+    Matrix4d A;
+    A(3, 3) = 1.0;
+    A(3, 2) = 0.0;
+    A(3, 1) = 0.0;
+    A(3, 0) = 0.0;
+
+    A(0, 0) = cos(theta);
+    A(0, 1) = -sin(theta)*cos(alpha);
+    A(0, 2) = sin(theta)*sin(alpha);
+    A(0, 3) = a * cos(theta);
+
+    A(1, 0) = sin(theta);
+    A(1, 1) = cos(theta)*cos(alpha);
+    A(1, 2) = -cos(theta)*sin(alpha);
+    A(1, 3) = a * sin(theta);
+
+    A(2, 0) = 0.0;
+    A(2, 1) = sin(alpha);
+    A(2, 2) = cos(alpha);
+    A(2, 3) = d;
+
+    return A;
+}
+
+Matrix4d youbot_kinematic::forward_kine(double joint_val[], int frame)
+{
+
+    Matrix4d A;
+    Matrix4d T = Matrix4d::Identity(4, 4);
+
+    for(int i = 0;i < frame; i++)
+    {
+        A = dh_matrix_standard(DH_params[i][0], DH_params[i][1], DH_params[i][2], joint_val[i] + DH_params[i][3]);
+
+        T = T * A;
+    }
+
+    return T;
+}
+
+Matrix4d youbot_kinematic::forward_kine_offset(double joint_val[], int frame)
+{
+
+    Matrix4d A;
+    Matrix4d T = Matrix4d::Identity(4, 4);
+
+    for(int i = 0;i < frame; i++)
+    {
+        if (i == 0)
+            A = dh_matrix_standard(DH_params[i][0], DH_params[i][1], DH_params[i][2], joint_offset[i] - (joint_val[i] + DH_params[i][3]));
+        else
+            A = dh_matrix_standard(DH_params[i][0], DH_params[i][1], DH_params[i][2], (joint_val[i] + DH_params[i][3]) - joint_offset[i]);
+
+        T = T * A;
+    }
+
+    return T;
+}
+
+void youbot_kinematic::broadcast_pose(Matrix4d pose)
+{
+
+    Eigen::Affine3d pose_affine;
+    pose_affine.matrix() = pose;
+
+    geometry_msgs::TransformStamped T = tf2::eigenToTransform(pose_affine);
+
+    T.header.stamp = ros::Time::now();
+    T.header.frame_id = "base_link";
+    T.child_frame_id = "arm_end_effector";
+
+    pose_br.sendTransform(T);
+}
+
+VectorXd youbot_kinematic::obtain_pose_vector(Matrix4d pose_matrix) {
+    VectorXd pose_vector(6);
+    double target_angle = acos((pose_matrix(0,0) + pose_matrix(1,1) + pose_matrix(2,2) - 1)/2);
+    pose_vector(0) = pose_matrix(0, 3);
+    pose_vector(1) = pose_matrix(1, 3);
+    pose_vector(2) = pose_matrix(2, 3);
+    if (sin(target_angle) != 0)
+    {
+        pose_vector(3) = (1/(2*sin(target_angle))) * (pose_matrix(2,1) - pose_matrix(1,2));
+        pose_vector(4) = (1/(2*sin(target_angle))) * (pose_matrix(0,2) - pose_matrix(2,0));
+        pose_vector(5) = (1/(2*sin(target_angle))) * (pose_matrix(1,0) - pose_matrix(0,1));
+    }
+    else
+    {
+        pose_vector(3) = 0;
+        pose_vector(4) = 0;
+        pose_vector(5) = 0;
+    }
+    return pose_vector;
+}
+
+MatrixXd youbot_kinematic::get_jacobian(double joint_val[])
+{
+    MatrixXd jacobian(6,5);
+    Matrix4d* forw_kin_matr = new Matrix4d[6];
+    Vector3d* z_i = new Vector3d[6];
+    Vector3d* o_i = new Vector3d[6];
+    for (int frame = 0; frame <= 5; frame++) 
+    {
+        forw_kin_matr[frame] = forward_kine(joint_val, frame);
+        z_i[frame] = forw_kin_matr[frame].block(0,2,3,1);
+        o_i[frame] = forw_kin_matr[frame].block(0,3,3,1);
+    }
+    for (int frame = 1; frame <= 5; frame++) 
+    {
+        Vector3d J_pi = z_i[frame - 1].cross(o_i[5] - o_i[frame - 1]);
+        Vector3d J_oi = z_i[frame - 1];
+        for (int pos = 0; pos < 3; pos++) 
+        {
+            jacobian(pos, frame - 1) = J_pi(pos);
+            jacobian(3 + pos, frame - 1) = J_oi(pos);
+        }
+    }
+    return jacobian;
+}
+
+MatrixXd youbot_kinematic::inverse_kine_closed_form(Matrix4d pose)
+{
+    //Ignore this.
+}
+
+double* youbot_kinematic::inverse_kine_ite(Matrix4d pose, double joint_val[])
+{
+    double gamma = 0.6;
+    VectorXd target_pose(6);
+    VectorXd current_pose(6);
+    double error_var = 0.000001;
+    target_pose = obtain_pose_vector(pose);
+    double* joint_val_post = new double[5];
+    joint_val_post[0] = joint_val[0]; joint_val_post[1] = joint_val[1]; joint_val_post[2] = joint_val[2];
+    joint_val_post[3] = joint_val[3]; joint_val_post[4] = joint_val[4];
+    double vari = 1;
+    double pose_error = 1, pose_error_prev = 0;
+    VectorXd joint_val_post_aux(5);
+    joint_val_post_aux << joint_val_post[0], joint_val_post[1], joint_val_post[2], joint_val_post[3], joint_val_post[4];
+    int iter_n = 0;
+    while (error_var < vari)
+    {
+        Matrix4d current_pose_matrix = forward_kine(joint_val_post, 5);
+        current_pose = obtain_pose_vector(current_pose_matrix);
+        pose_error_prev = pose_error;
+        if (check_singularity(joint_val_post))
+            std::cout << "Is singularity?: " << check_singularity(joint_val_post) << std::endl;
+        MatrixXd jacobian = get_jacobian(joint_val_post);
+        MatrixXd Ide = MatrixXd::Identity(5, 5);
+        MatrixXd fu = (jacobian.transpose()*jacobian + (gamma*gamma)*Ide).inverse()*jacobian.transpose();
+        if (iter_n > 10000)
+        {
+            break;
+        }
+        joint_val_post_aux = joint_val_post_aux + fu*(target_pose - current_pose);
+        joint_val_post[0] = joint_val_post_aux(0); joint_val_post[1] = joint_val_post_aux(1); 
+        joint_val_post[2] = joint_val_post_aux(2); joint_val_post[3] = joint_val_post_aux(3); 
+        joint_val_post[4] = joint_val_post_aux(4); 
+        pose_error = (target_pose - current_pose).norm();
+        vari = abs(pose_error - pose_error_prev);
+        iter_n++;
+    }
+    std::cout << "Joint N: " << joint_val_post_aux * 180/M_PI << std::endl;
+    return joint_val_post;
+}
+
+double* youbot_kinematic::fix_limits(double joint_val[])
+{
+    bool limit_reached = false;
+    for (int i = 0; i < 5; i++) 
+    {
+        if (joint_val[i] > joint_limit_max[i] || joint_val[i] < joint_limit_min[i]) 
+            limit_reached = true;
+            double interval = joint_limit_max[i] - joint_limit_min[i];
+            double rand_num = ((double) rand() / (RAND_MAX));
+            joint_val[i] = rand_num*interval + joint_limit_min[i];
+    } 
+    if (limit_reached){
+        for (int i = 0; i < 5; i++){
+            double interval = joint_limit_max[i] - joint_limit_min[i];
+            double rand_num = ((double) rand() / (RAND_MAX));
+            joint_val[i] = rand_num*interval + joint_limit_min[i];
+        }
+    }
+}
+
+double* youbot_kinematic::norm_angle(double joint_val[]){
+    double* new_joint = new double[5];
+    for (int i = 0; i < 5; i++) 
+    {
+        if (joint_val[i] > 3.14)
+            int t =1;
+        int a = (int)joint_val[i]/(2*M_PI);
+        new_joint[i] = joint_val[i] - ((double)a) * (M_PI);
+        if (new_joint[i] > M_PI) {
+            new_joint[i] = new_joint[i] - 2*M_PI;
+        }
+        else if(new_joint[i] < -M_PI) {
+            new_joint[i] = new_joint[i] + 2*M_PI;
+        }
+    }
+    return new_joint;
+}
+
+bool youbot_kinematic::check_singularity(double joint_val[])
+{
+    MatrixXd jacobian = get_jacobian(joint_val);
+    MatrixXd Ide = MatrixXd::Identity(5, 5);
+    MatrixXd inverse = jacobian.transpose()*jacobian;
+    return inverse.determinant() == 0;
+}
+
+double* youbot_kinematic::apply_offset(double joint_val[]) {
+    double *new_joint = new double[5];
+    new_joint[0] = joint_offset[0] - joint_val[0]; 
+    new_joint[1] = joint_val[1] + joint_offset[1];
+    new_joint[2] = joint_val[2] + joint_offset[2];
+    new_joint[3] = joint_val[3] + joint_offset[3];
+    new_joint[4] = joint_val[4] + joint_offset[4];
+
+    return new_joint;
+}
